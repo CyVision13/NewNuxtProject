e9e8ee21f1786bb588212cb4d92043ad2f2dabca
CyVision13 <CyVision13@outlook.com>
2020-10-19 10:20:59 -0400

    Creating the Main Sections (Pages)

---
+++ FP/pages/about/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="about-page">
+        <h1>This is my absolutely fantastic "About Me" Page!</h1>
+        <p>Lore ipsum, is a good shit</p>
+        <p>Lore ipsum, is a good shit</p>
+        <p>Lore ipsum, is a good shit</p>
+        <p>Lore ipsum, is a good shit</p>
+        
+    </div>
+</template>
+
+<style scoped>
+.about-page {
+    text-align: center;
+    padding: 30px;
+}
+</style>
