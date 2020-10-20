b97f7120d684ad9852099443140fd7da42459099
CyVision13 <CyVision13@outlook.com>
2020-10-20 05:13:06 -0400

    Adding External Fonts

--- FP/nuxt.config.js
+++ FP/nuxt.config.js
@@ -8,7 +8,8 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel:'stylesheet',href: "https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@1,300&display=swap"}
     ]
   },
 
