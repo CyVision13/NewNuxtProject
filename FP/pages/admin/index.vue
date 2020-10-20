1b4caa1a871b8799d8c7781860cb6f0b763f4d8c
CyVision13 <CyVision13@outlook.com>
2020-10-20 12:20:11 -0400

    Creating an Admin Section

---
+++ FP/pages/admin/index.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="admin-page">
+        <section class="new-post">
+            <button @click="$router.push('/admin/new-post')">Create Post</button>
+
+        </section>
+        <section class="existing-posts">
+            <h1>Existing Posts</h1>
+            <PostList />
+        </section>
+    </div>
+</template>
+
+<script>
+import PostList from '@/components/posts/PostList'
+
+export default {
+    components: {
+        PostList
+    }
+    
+}
+</script>
+
+
+<style  scoped>
+.admin-page {
+    padding: 20px;
+
+}
+
+.new-post{
+    text-align: center;
+    border-bottom: 2px solid #ccc;
+    padding-bottom: 10px;
+}
+
+.existing-posts h1 {
+    text-align: center;
+}
+
+</style>
---
+++ FP/pages/admin/new-post/index.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="admin-new-post-page">
+        <section class="new-post-form">
+            <form @submit.prevent="onSave">
+                <AppControlInput v-model="editedPost.author">Author Name</AppControlInput>
+                <AppControlInput v-model="editedPost.title">Title</AppControlInput>
+                <AppControlInput v-model="editedPost.thumbnailLink">thumbnail Link</AppControlInput>
+                <AppControlInput 
+                    control-type="textarea"
+                v-model="editedPost.content">Content</AppControlInput>
+                <AppButton type="submit">Save </AppButton>
+                <AppButton 
+                    type="button"
+                    style="margin-left : 10px"
+                    btn-style="cancel"
+                    @click="onCancel"
+                > Cancle </AppButton>
+
+            </form>
+        </section>
+    </div>
+</template>
+
+<script>
+import AppControlInput from '~/components/UI/AppControlInput.vue'
+import AppButton from '~/components/UI/AppButton.vue'
+export default {
+    components:{
+        AppControlInput,
+        AppButton
+    },
+    data(){
+        return {
+            editedPost :{
+                author:'',
+                thumbnailLink: ''
+            }
+        }
+    },
+    methods: {
+        onSave(){
+            // Save the post
+            console.log(this.editedPost)
+        },
+        onCancel(){
+            // Navigate back
+            this.$router.push('/admin')
+        }
+    }
+}
+</script>
