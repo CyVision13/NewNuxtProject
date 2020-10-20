2a67cd7a9fb844616e517eb30ddd2c1d6895f9cb
CyVision13 <CyVision13@outlook.com>
2020-10-20 14:58:35 -0400

    Preparing the Backend

---
+++ FP/components/Admin/AdminPostForm.vue
@@ -0,0 +1,47 @@
+<template>
+    <form @submit.prevent="onSave">
+                <AppControlInput v-model="editedPost.author">Author Name</AppControlInput>
+                <AppControlInput v-model="editedPost.title">Title</AppControlInput>
+                <AppControlInput v-model="editedPost.thumbnailLink">thumbnail Link</AppControlInput>
+                <AppControlInput 
+                    control-type="textarea"
+                v-model="editedPost.content">Content</AppControlInput>
+                <AppButton type="submit">Save </AppButton>
+                <AppButton 
+                    type="button"
+                    style="margin-left : 10px"
+                    btn-style="cancel"
+                    @click="onCancel"
+                > Cancle </AppButton>
+
+            </form>
+</template>
+
+<script>
+import AppControlInput from '~/components/UI/AppControlInput.vue'
+import AppButton from '~/components/UI/AppButton.vue'
+export default {
+    components:{
+        AppControlInput,
+        AppButton
+    },
+    data(){
+        return {
+            editedPost :{
+                author:'',
+                thumbnailLink: ''
+            }
+        }
+    },
+    methods: {
+        onSave(){
+            // Save the post
+            console.log(this.editedPost)
+        },
+        onCancel(){
+            // Navigate back
+            this.$router.push('/admin')
+        }
+    }
+}
+</script>
---
+++ FP/pages/admin/_postId/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="admin-post-page">
+        <section class="update-form">
+            <AdminPostForm/>
+        </section>
+    </div>
+</template>
+<script>
+import AdminPostForm from '@/components/Admin/AdminPostForm'
+export default {
+    components:{
+        AdminPostForm
+    },
+    data(){
+        return {
+            loadedPost:{
+                author: "reza",
+                title:'My Awesome Post',
+                content: "Super amazing thanks for that!",
+                thumbnailLink:"https://image.shutterstock.com/image-vector/abstract-lines-dots-connect-background-600w-1492332182.jpg "
+            }
+        }
+    }
+}
+</script>
