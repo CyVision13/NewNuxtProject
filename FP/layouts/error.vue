4d007d0e553b86b08439377a5ef4fe91de7132db
CyVision13 <CyVision13@outlook.com>
2020-10-21 04:42:35 -0400

    Adding a Separate Layout to the Admin Area

--- FP/layouts/error.vue
+++ FP/layouts/error.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="error-page">
     <h1>Oops, something went wrong!</h1>
+
+    <!-- we used a tag cz if app crash, we cant access to js to use nuxt link -->
     <p>Back to <a href="/">safety</a>!</p>
   </div>
 </template>
