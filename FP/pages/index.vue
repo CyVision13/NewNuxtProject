8e9e9f491f04afe4bb9f71d1d806b9eb1f4ae390
CyVision13 <CyVision13@outlook.com>
2020-10-24 04:31:54 -0400

    Vuex, fetch() and nuxtServerInit()

--- FP/pages/index.vue
+++ FP/pages/index.vue
@@ -14,35 +14,10 @@ export default {
   components: {
     PostList
   },
-  asyncData(context, callback){
-    setTimeout(()=> {
-     callback(null, {
-       loadedPosts : [
-        {
-          id:"1",
-          title:"first post",
-          previewText:"this is our first post!",
-          thumbnail:"https://image.shutterstock.com/image-vector/abstract-lines-dots-connect-background-600w-1492332182.jpg"
-        },
-        {
-          id:"2",
-          title:"Second post",
-          previewText:"this is our Second post!",
-          thumbnail:"https://image.shutterstock.com/image-vector/abstract-lines-dots-connect-background-600w-1492332182.jpg"
-        },
-        {
-          id:"3",
-          title:"Third post",
-          previewText:"this is our Third post!",
-          thumbnail:"https://image.shutterstock.com/image-vector/abstract-lines-dots-connect-background-600w-1492332182.jpg"
-        }
-      ] 
-     })
-       
-       
-       
-     
-    },1500)
+  computed:{
+    loadedPosts(){
+      return this.$store.getters.loadedPosts
+    }
   },
   // data(){
   //   return {
@@ -52,9 +27,7 @@ export default {
   //     ]
   //   }
   // },
-  created(){
-    console.log('shit');
-  }
+  
 }
 </script>
 
