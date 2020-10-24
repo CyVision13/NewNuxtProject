c70a7bd0ed95d872053eb84d11fe6f5c876650f6
CyVision13 <CyVision13@outlook.com>
2020-10-24 04:43:55 -0400

    Executing Code on the Server

--- FP/store/index.js
+++ FP/store/index.js
@@ -13,6 +13,9 @@ const createStore = ()=>{
         },
         actions: {
             nuxtServerInit(vuexContext,context){
+                // if(!process.client){
+                //     console.log(context.req.session);
+                // }
                 return new Promise((resolve,reject)=>{
                     setTimeout(()=> {
                         vuexContext.commit('setPosts', 
