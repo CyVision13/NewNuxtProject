e26e26ef238e4ef33ccd6fc215274a0a7a651fe3
CyVision13 <CyVision13@outlook.com>
2020-10-26 03:53:51 -0400

    The Nuxt Config File

--- FP/nuxt.config.js
+++ FP/nuxt.config.js
@@ -1,11 +1,11 @@
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'FP',
+    title: 'WD Blog',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: 'My Cool web blog' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
