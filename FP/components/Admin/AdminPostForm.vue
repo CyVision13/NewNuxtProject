028d68e1fdcb16c6fd020fbf48757af6d56e66dc
CyVision13 <CyVision13@outlook.com>
2020-10-28 01:59:08 -0400

    4. Using Firebase to Store Data

--- FP/components/Admin/AdminPostForm.vue
+++ FP/components/Admin/AdminPostForm.vue
@@ -46,7 +46,7 @@ export default {
     methods: {
         onSave(){
             // Save the post
-            console.log(this.editedPost)
+            this.$emit('submit',this.editedPost)
         },
         onCancel(){
             // Navigate back
--- FP/pages/admin/new-post/index.vue
+++ FP/pages/admin/new-post/index.vue
@@ -1,18 +1,26 @@
 <template>
     <div class="admin-new-post-page">
         <section class="new-post-form">
-            <AdminPostForm/>
+            <AdminPostForm @submit="onSubmitted"/>
         </section>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
 import AdminPostForm from '~/components/Admin/AdminPostForm.vue'
 export default {
     components :{
         AdminPostForm
     },
-    layout :'admin'
+    layout :'admin',
+    methods:{
+        onSubmitted(postData){
+            axios.post('https://vuejs-f4c7c.firebaseio.com/posts.json',postData)
+            .then(result => console.log(result))
+            .catch(e => console.log(e))
+        }
+    }
 }
 </script>
 
