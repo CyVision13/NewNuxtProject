b9367c13a4417a80c964a722ad9f503fd806ce19
cyvision13 <cyvision13@outlook.com>
2020-11-03 02:01:11 -0500

    Registering a Date Filter

--- FP/nuxt.config.js
+++ FP/nuxt.config.js
@@ -26,7 +26,8 @@ export default {
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
-    '~plugins/core-components.js'
+    '~plugins/core-components.js',
+    '~plugins/date-filter.js'
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
---
+++ FP/plugins/date-filter.js
@@ -0,0 +1,36 @@
+import Vue from 'vue'
+
+
+const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+
+]
+
+
+const dateFilter = value =>{
+    return formatDate(value)
+
+}
+
+function formatDate(inputDate){
+    const date = new Date(inputDate);
+    const year = date.getFullYear();
+    const month = date.getMonth();
+    const day = date.getDate();
+    const formattedDate = day +'. '+ months[month]+ ' '+year;
+    return formattedDate
+}
+
+
+Vue.filter('date',dateFilter)
