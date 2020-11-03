dc16883cee1c5c5c6956cd7db6ad6e0978543b7c
cyvision13 <cyvision13@outlook.com>
2020-11-03 03:26:15 -0500

    Fix transitions

--- FP/nuxt.config.js
+++ FP/nuxt.config.js
@@ -21,7 +21,7 @@ export default {
   // },
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
-    
+    '~assets/styles/main.css'
   ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
@@ -91,7 +91,7 @@ export default {
 
   // css for pages
   transition: {
-    name: 'page',
+    name: 'fade',
     mode:'out-in'
   }
 }
