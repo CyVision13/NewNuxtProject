eeb3c99cc4df99b71ec54da39cda178541687120
cyvision13 <cyvision13@outlook.com>
2020-11-03 09:46:23 -0500

    Adding User Login

--- FP/pages/admin/auth/index.vue
+++ FP/pages/admin/auth/index.vue
@@ -36,8 +36,14 @@ export default {
     },
     methods:{
         onSubmit(){
-            this.$axios.$post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key='
-            +process.env.fbAPIKey,{
+            let authUrl = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key='
+            +process.env.fbAPIKey
+            if(!this.isLogin) {
+                authUrl = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key='
+            +process.env.fbAPIKey
+            }
+            
+            this.$axios.$post(authUrl,{
                 email:this.email,
                 password:this.password,
                 returnSecureToken: true
@@ -46,6 +52,7 @@ export default {
                 console.log(result);
             })
             .catch(e => console.log(e))
+            
         }
     }
 }
