a47f4d84d0587f55435da5dbf80501e1ac4d9275
cyvision13 <cyvision13@outlook.com>
2020-11-08 03:38:32 -0500

    Building our App as a SPA

--- FP/nuxt.config.js
+++ FP/nuxt.config.js
@@ -2,7 +2,7 @@
 const bodyParser = require('body-parser')
 export default {
 
-  mode:'universal',
+  mode:'spa',
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: 'FP',
